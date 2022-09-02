e62d8abcd0bddd2e0434a0dbdece20c1ba4c9f97
Tollpatsch314 <76736177+Tollpatsch314@users.noreply.github.com>
2022-09-02 22:36:22 +0200

    OCCCHHH MANNNN!

--- Aufgabe 5/main.cpp
+++ Aufgabe 5/main.cpp
@@ -83,24 +83,66 @@ int main(int argc, char** argv) {
 	auto calc_possible_games = [](Graph& graph) {
 
 		std::vector<size_t> sect1, sect2;					// they store the shortest path to the "root"
-		std::vector<size_t> steps, intersection {  };		// = sect1\cap sect2
-
+		std::vector<size_t> intersection {  };				// = sect1\cap sect2
+		size_t min_count;
 
 		auto find_min_steps = [&]() {
 
+			size_t
+				next,
+				counter1,
+				counter2;
+
+			size_t
+				iterator = 0,
+				min_count_index = 0;
+
+			min_count = SIZE_MAX;
+
 			// count the steps, which are needed to move to a node from to nodes
-			for (auto possible_end : intersection) {
+			for (; iterator < intersection.size(); iterator++) {
+				
+				std::cout << "\npossible_end : " << intersection[iterator] + 1 << "\n";
 
-				size_t next = possible_end, counter = 0;
-				while ((next = sect1[next]) != BFS_NOT_VISITED) counter++;					// count the first section path (by moving to the "root"-element)
+				next = intersection[iterator],
+				counter1 = 0,
+				counter2 = 0;
 
-				steps.push_back(counter);			// "save" counted value
-				counter = 0;							// ... and reset!
+				while ((next = sect1[next]) != BFS_NOT_VISITED) counter1++;					// count the first section path (by moving to the "root"-element)
+				next = intersection[iterator];
+				while ((next = sect2[next]) != BFS_NOT_VISITED) counter2++;					// ... and the second section path
 
-				while ((possible_end = sect2[possible_end]) != BFS_NOT_VISITED) counter++;	// count the second section path
+				if (counter1 < counter2) counter1 = counter2;								// the maximum is of course required
+				if (counter1 < min_count) {
+					min_count = counter1;
+					min_count_index = iterator;
+				}
 
-				steps.back() = std::max(steps.back(), counter);					// the maximum is of course required
+				iterator++;
 			}
+
+			return min_count_index;
+		};
+
+		auto redef_sect_lists = [&](size_t node_index) {
+
+			std::vector<size_t> tmp;
+			size_t node_index2 = node_index;
+
+			while (sect1[node_index] != BFS_NOT_VISITED) {
+				tmp.push_back(node_index);
+				node_index = sect1[node_index];
+			}
+
+			sect1 = tmp;
+			tmp.clear();
+
+			while (sect2[node_index2] != BFS_NOT_VISITED) {
+				tmp.push_back(node_index2);
+				node_index2 = sect2[node_index2];
+			}
+
+			sect2 = tmp;
 
 		};
 
@@ -114,10 +156,23 @@ int main(int argc, char** argv) {
 				intersection.push_back(t);
 		}
 
-		find_min_steps();
+		redef_sect_lists(find_min_steps());
 
-		return std::make_tuple(intersection, steps);
+		return std::make_tuple(sect1, sect2, min_count);
 	};
+
+	auto [sect1, sect2, min_count] = calc_possible_games(graph);
+
+	std::cout << "minimum possible move count : " << min_count;
+	std::cout << "\nsteps for player on position 1 : \n";
+
+	for (auto it = sect1.begin(); it != sect1.end(); ++it)
+		std::cout << "\t" << *it + 1 << " <==\n";
+
+	std::cout << "\nsteps for player on position 2 : \n";
+
+	for (auto it = sect2.begin(); it != sect2.end(); ++it)
+		std::cout << "\t" << *it + 1 << " <==\n";
 
 	return 0;
 }
